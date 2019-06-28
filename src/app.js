a809fba904b12430783c631375869595a87f8b8f
xiaoling.wang <xiaoling.wang@weimob.com>
2019-06-28 18:51:06 +0800

    feat:project base config

--- src/app.js
+++ src/app.js
@@ -1,15 +1,20 @@
-import React from 'react';
-import { Router, Route, Switch } from 'react-router-dom';
+import React from "react";
+import { Router, Route, Switch } from "react-router-dom";
 import history from "@/utils/history";
+import { Goods, Users, Dashboard } from "@/router";
+
+console.log(Goods, 4545)
 
 const App = () => {
-   return (
+  return (
     <Router history={history}>
-        <Switch>
+      <Switch>
         <Route path="/" exact component={Dashboard} />
         <Route path="/users" exact component={Users} />
         <Route path="/goods" exact component={Goods} />
-        </Switch>
+      </Switch>
     </Router>
-   )
-}
+  );
+};
+
+export default App;
---
+++ src/theme.less.js
@@ -0,0 +1,9 @@
+module.exports = {
+    '@font-size-base': '12px',
+    '@font-family': 'Microsoft YaHei, Helvitica, Verdana, Arial, san-serif',
+    '@primary-color': '#2589ff',
+    '@layout-header-background': '#222430',
+    '@menu-dark-color': '#757993',
+    '@menu-dark-bg': '#222430',
+};
+  
